68c17bcb76112f25df9432afc9802bb10a63c0bd
danar <danarvare2003@gmail.com>
2023-03-09 20:05:12 -0600

    Termine mi chamba 5

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ function App() {
       <Buscador></Buscador>
       </div>
       <div>
-      <MainInfoBlocks></MainInfoBlocks>
+      <div className='fondo'>
+        <svg class="content-info__curve" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1438 134"><path d="M1438 1442H0V31.001S438.105 0 719 .001c280.896 0 719 31 719 31V1442z" fill="currentColor"></path></svg>
+      </div>
+
+      <MainInfoBlocks className ='mainInfo'></MainInfoBlocks>
       </div>
       
       
