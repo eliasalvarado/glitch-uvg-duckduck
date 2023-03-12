78ba791484391b8ee3478fe53a75d210df86522a
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-03-12 00:53:31 -0600

    Cambios Finales LastImage

--- src/components/LastImage/LastImage.jsx
+++ src/components/LastImage/LastImage.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './LastImage.css'
 
+
 const LastImage = () => {
     const [count, setCount] = useState(0)
     return (
         <div className='LastImage'>
-            <img src="https://duckduckgo.com/assets/onboarding/bathroomguy/1-monster-v2--pre-animation.svg"></img>
+            <img width="1200" height="460" src="https://duckduckgo.com/assets/onboarding/bathroomguy/1-monster-v2--pre-animation.svg"/>
         </div>
     )
 }
